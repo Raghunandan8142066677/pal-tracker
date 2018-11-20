73f0c66d4911e3490b938e9b434b81613fdcbb20
sashinpivotal <sashin@pivotal.io>
2018-11-20 16:38:20 -0500

    Health

--- src/test/java/test/pivotal/pal/trackerapi/TimeEntryApiTest.java
+++ src/test/java/test/pivotal/pal/trackerapi/TimeEntryApiTest.java
@@ -1,5 +1,5 @@
-package test.pivotal.pal.trackerapi;
 
+package test.pivotal.pal.trackerapi;
 import com.jayway.jsonpath.DocumentContext;
 import com.mysql.cj.jdbc.MysqlDataSource;
 import io.pivotal.pal.tracker.PalTrackerApplication;
